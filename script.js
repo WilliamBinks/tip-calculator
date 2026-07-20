7c4c966129e7c17ca38560c477843d795df12c73
William Binks <williamjbinks@gmail.com>
2026-07-20 22:16:30 +0100

    added validation/support

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let tipAmount;
 let total;
 let calculateButton = document.getElementById("calculate");
 let outputBox = document.getElementById("final-cost");
+let billAmountError = document.getElementById("bill-amount-error");
+let tipError = document.getElementById("tip-error");
+
 
 tenPercentButton.addEventListener("click",() => buttonPressed(10));
 fifteenPercentButton.addEventListener("click",() => buttonPressed(15));
@@ -17,14 +20,29 @@ calculateButton.addEventListener("click",() => calculate(tipSelected))
 
 function buttonPressed(button){
     tipSelected = button;
+    tipError.textContent = ""
 }
 
 function calculate(percentageChoice){
     userInput = userInputBox.value;
-    percentageChoice = percentageChoice/100;
-    tipAmount = userInput * percentageChoice;
-    console.log(tipAmount);
-    total = Number(userInput) + Number(tipAmount);
-    console.log(total);
-    outputBox.textContent = "$"+String(total);
+    if (userInput && percentageChoice){
+        percentageChoice = percentageChoice/100;
+        tipAmount = userInput * percentageChoice;
+        total = Number(userInput) + Number(tipAmount);
+        outputBox.textContent = "$"+String(total);
+        billAmountError.textContent= ""
+        tipError.textContent = ""
+    } else{
+        console.log(percentageChoice);
+        console.log(typeof userInput);
+        if (userInput === "" && percentageChoice !== undefined){
+            billAmountError.textContent = "Please enter the bill amount";
+        }else if(userInput !== "" && percentageChoice === undefined){
+            tipError.textContent = "Please select the tip";
+        }else{
+            billAmountError.textContent = "Please enter the bill amount";
+            tipError.textContent = "Please select the tip";
+        }   
+    }
+    
 }
