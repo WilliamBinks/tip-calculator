bf15f193637ec87966a51cdda5dd68aa1e9e5da7
William Binks <williamjbinks@gmail.com>
2026-07-20 15:38:59 +0100

    Added tipping logic

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 let userInputBox = document.getElementById("bill-amount");
 let userInput;
-let tenPercent = document.getElementById("ten");
-let fifteenPercent = document.getElementById("fifteen");
-let twentyPercent = document.getElementById("twenty");
+let tenPercentButton = document.getElementById("ten");
+let fifteenPercentButton = document.getElementById("fifteen");
+let twentyPercentButton = document.getElementById("twenty");
 let tipSelected;
+let tipAmount;
+let total;
+let calculateButton = document.getElementById("calculate");
+let outputBox = document.getElementById("final-cost");
+
+tenPercentButton.addEventListener("click",() => buttonPressed(10));
+fifteenPercentButton.addEventListener("click",() => buttonPressed(15));
+twentyPercentButton.addEventListener("click",() => buttonPressed(20));
+
+calculateButton.addEventListener("click",() => calculate(tipSelected))
 
 function buttonPressed(button){
-    console.log(button);
+    tipSelected = button;
 }
 
-tenPercent.addEventListener("click",buttonPressed(tenPercent));
-
-
+function calculate(percentageChoice){
+    userInput = userInputBox.value;
+    percentageChoice = percentageChoice/100;
+    tipAmount = userInput * percentageChoice;
+    console.log(tipAmount);
+    total = Number(userInput) + Number(tipAmount);
+    console.log(total);
+    outputBox.textContent = "$"+String(total);
+}
